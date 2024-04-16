6224521ff8f2a70ca75e0b3f13f040e828221b10
ezh <e.v.zhukova919@gmail.com>
2024-04-16 19:45:23 +0300

    update test_textbox

--- AT_qa-practice2/tests/test_textbox2.py
+++ AT_qa-practice2/tests/test_textbox2.py
@@ -1,22 +1,15 @@
-# from base.base_test import BaseTest
 import allure
 import pytest
 from pages.textbox_page import TextboxPage
 
 
-# @allure.testcase("TBX functionality")
-# class TestTextBox(BaseTest):
-    
-    # @allure.title("TBX testcase title")
-    # @allure.severity("High")
-    # @pytest.mark.smoke
 def test_textbox(driver):
-        test_page = TextboxPage(driver)  # в этой строке указываем для test_page куда ей обращаться 
-        test_page.open()                 # -> class (TextboxPage) в соответсвующем _page.py файле
-        test_page.is_opened()
-        test_page.textbox_present()
-        test_page.textbox_clear()
-        test_page.textbox_input_value()
-        test_page.submit()
-
-
+    test_page = TextboxPage(
+        driver
+    )  # в этой строке указываем для test_page куда ей обращаться
+    test_page.open()  # -> class (TextboxPage) в соответсвующем _page.py файле
+    test_page.is_opened()
+    test_page.textbox_present()
+    test_page.textbox_clear()
+    test_page.textbox_input_value()
+    test_page.submit()
